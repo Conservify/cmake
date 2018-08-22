a6dc2ff2383dce9b2a19111741e93ff638377688
Jacob Lewallen <jlewallen@gmail.com>
2018-08-21 17:08:08 -0700

    Much better transitive library handling.

--- FindFkModule.cmake
+++ FindFkModule.cmake
@@ -20,30 +20,35 @@ add_definitions("-DFIRMWARE_BUILD=\"$ENV{BUILD_TAG}\"")
 if (NOT TARGET firmware-common)
   add_external_arduino_library(firmware-common)
 
-  find_package(arduino-logging)
-  find_package(lwcron)
-  find_package(RTCZero)
-  find_package(RTClib)
-  find_package(phylum)
-  find_package(module-protocol)
-  find_package(app-protocol)
-  find_package(data-protocol)
-  find_package(lwstreams)
-  find_package(WiFi101)
+  find_package(nanopb)
+  target_link_libraries(firmware-common nanopb)
 
-  target_link_libraries(firmware-common arduino-logging)
+  find_package(lwcron)
   target_link_libraries(firmware-common lwcron)
+
+  find_package(RTCZero)
   target_link_libraries(firmware-common RTCZero)
+
+  find_package(RTClib)
   target_link_libraries(firmware-common RTClib)
 
+  find_package(phylum)
   target_compile_options(phylum PUBLIC -DPHYLUM_ENABLE_SERIAL_FLASH -DPHYLUM_ENABLE_SD)
-
   target_link_libraries(firmware-common phylum)
 
+  find_package(module-protocol)
   target_link_libraries(firmware-common module-protocol)
+
+  find_package(app-protocol)
   target_link_libraries(firmware-common app-protocol)
+
+  find_package(data-protocol)
   target_link_libraries(firmware-common data-protocol)
+
+  find_package(lwstreams)
   target_link_libraries(firmware-common lwstreams)
+
+  find_package(WiFi101)
   target_link_libraries(firmware-common WiFi101)
 
   find_package(FuelGauge)
@@ -57,25 +62,24 @@ if (NOT TARGET firmware-common)
 
   find_package(AtSamd)
   target_link_libraries(firmware-common AtSamd)
+
+  find_package(arduino-logging)
+  target_link_libraries(firmware-common arduino-logging)
+
+  find_package(SerialFlash)
+  target_link_libraries(firmware-common SerialFlash)
+
+  find_package(Wire)
+  target_link_libraries(firmware-common Wire)
+
+  find_package(SPI)
+  target_link_libraries(firmware-common SPI)
 endif()
 
 function(fk_configure_module target_name)
   message("Configuring FkModule: ${target_name}")
 
-  find_package(SPI)
-  find_package(Wire)
-
   target_link_libraries(${target_name} firmware-common)
-  target_link_libraries(${target_name} RTClib)
-  target_link_libraries(${target_name} Wire)
-  target_link_libraries(${target_name} RTCZero)
-  target_link_libraries(${target_name} phylum)
-  target_link_libraries(${target_name} arduino-logging)
-  target_link_libraries(${target_name} AtSamd)
-  target_link_libraries(${target_name} nanopb)
-  target_link_libraries(${target_name} module-protocol)
-  target_link_libraries(${target_name} SerialFlash)
-  target_link_libraries(${target_name} SPI)
 
   target_include_directories(${target_name} PRIVATE ./)
 endfunction()
--- Findphylum.cmake
+++ Findphylum.cmake
@@ -8,13 +8,14 @@ set(phylum_RECURSE True)
 
 add_external_arduino_library(phylum)
 
-find_package(SPI)
 find_package(arduino-logging)
-find_package(SerialFlash)
-
-target_link_libraries(phylum SPI)
 target_link_libraries(phylum arduino-logging)
+
+find_package(SerialFlash)
 target_link_libraries(phylum SerialFlash)
+
+find_package(SPI)
+target_link_libraries(phylum SPI)
 
 target_include_directories(phylum
     PUBLIC ${phylum_PATH}/src

a5a424d2f76a18b72e108e7b432606721a85afdc
Jacob Lewallen <jlewallen@gmail.com>
2019-06-29 11:49:31 -0700

    Work with non Arduino builds.

--- Findarduino-logging.cmake
+++ Findarduino-logging.cmake
@@ -2,4 +2,12 @@ if (TARGET arduino-logging)
   return()
 endif()
 
-add_external_arduino_library(arduino-logging)
+if(${TARGET_ARCH} MATCHES "amd64")
+  file(GLOB sources ${arduino-logging_PATH}/src/*.cpp)
+
+  add_library(arduino-logging STATIC ${sources})
+
+  target_include_directories(arduino-logging PUBLIC ${arduino-logging_PATH}/src)
+else()
+  add_external_arduino_library(arduino-logging)
+endif()

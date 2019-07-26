f0ec9ed0918f00f9af85e9fcd92ff25e526eaddc
Jacob Lewallen <jlewallen@gmail.com>
2019-07-25 18:50:20 -0700

    Add conventional setup for lwcron.

--- Findlwcron.cmake
+++ Findlwcron.cmake
@@ -2,6 +2,16 @@ if(TARGET lwcron)
   return()
 endif()
 
-set(lwcron_RECURSE True)
+if(${TARGET_ARCH} MATCHES "amd64")
+  file(GLOB sources ${lwcron_PATH}/src/lwcron/*.cpp)
 
-add_external_arduino_library(lwcron)
+  add_library(lwcron STATIC ${sources})
+
+  target_include_directories(lwcron
+    PUBLIC ${lwcron_PATH}/src
+    PRIVATE ${lwcron_PATH}/src
+  )
+else()
+  set(lwcron_RECURSE True)
+  add_external_arduino_library(lwcron)
+endif()

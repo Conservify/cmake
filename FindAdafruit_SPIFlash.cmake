8adc0bdb29bd789d5582d55272ec3dc3c04a2a77
Jacob Lewallen <jlewallen@gmail.com>
2019-08-20 22:56:10 -0700

    Add SdFat dependency to SPIFlash.

--- FindAdafruit_SPIFlash.cmake
+++ FindAdafruit_SPIFlash.cmake
@@ -2,8 +2,17 @@ if (TARGET Adafruit_SPIFlash)
   return()
 endif()
 
+set(Adafruit_SPIFlash_RECURSE YES)
+
 add_external_arduino_library(Adafruit_SPIFlash)
 
-find_package(SPI)
+target_include_directories(Adafruit_SPIFlash
+  PUBLIC ${lwcron_PATH}/src ${lwcron_PATH}/src/qspi ${lwcron_PATH}/src/spi
+  PRIVATE ${lwcron_PATH}/src ${lwcron_PATH}/src/qspi ${lwcron_PATH}/src/spi
+)
 
+find_package(SPI)
 target_link_libraries(Adafruit_SPIFlash SPI)
+
+find_package(SdFat)
+target_link_libraries(Adafruit_SPIFlash SdFat)
